18d6acf701d01eee1ab85ea8e2282029cecae270
Leandro Reis <developer.reis@gmail.com>
2024-07-02 18:09:14 +0100

    twosums

--- src/main/java/com/lnreis/challenges/TwoSums.java
+++ src/main/java/com/lnreis/challenges/TwoSums.java
@@ -1,7 +1,29 @@
 package com.lnreis.challenges;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSums {
     public static void main(String[] args) {
-        System.out.println("Two Sums");
+        int[] numbers = new int[] {2, 3, 7, 4, 8};
+        int target = 7;
+        var result = getTwoSum(numbers, target);
+        if(result != null) {
+            System.out.println(STR."\{result[0]}, \{result[1]}");
+        } else {
+            System.out.println("No numbers found!");
+        }
+    }
+
+    private static int[] getTwoSum(int[] numbers, int target){
+        Map<Integer, Integer> visitedNumbers = new HashMap<>();
+        for(int i = 0; i < numbers.length; i++){
+            int delta = target - numbers[i];
+            if(visitedNumbers.containsKey(delta)) {
+                return new int[] {i, visitedNumbers.get(delta)};
+            }
+            visitedNumbers.put(numbers[i], i);
+        }
+        return null;
     }
 }
